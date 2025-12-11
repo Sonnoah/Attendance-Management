20acd6c2bdada3d3c2d498d8f51b0d06ac3de372
Sonnoah <Dllight1031@gmail.com>
2025-12-11 15:38:44 +0700

    commit

--- script/savedata.js
+++ script/savedata.js
@@ -1,10 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
 
-import { 
-getFirestore, 
-collection, 
-addDoc 
+import { getFirestore, collection, addDoc 
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
